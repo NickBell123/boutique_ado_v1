36f1690ce6de8190e7581bd686da3cd308930974
NickBell123 <nick.bell2008@gmail.com>
2020-09-23 07:22:27 +0000

    stripe lesson 4

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -26,3 +26,20 @@ var style = {
 };
 var card = elements.create('card', );
 card.mount('#card-element');
+
+//Handle realtimevalidation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error){
+        var html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
