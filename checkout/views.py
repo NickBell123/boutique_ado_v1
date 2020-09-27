6ec108e2133d24b99b3c6d89ea47f0b93bcc316f
NickBell123 <nick.bell2008@gmail.com>
2020-09-27 07:42:30 +0000

    completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,7 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
+
 @require_POST
 def cache_checkout_data(request):
     try:
@@ -26,6 +27,7 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+        
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -47,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -68,10 +74,10 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(request, (
+                    messages.error(request,
                         "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
-                    )
+
                     order.delete()
                     return redirect(reverse('view_bag'))
 
